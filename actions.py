4ad90ffe109323cefd4ef381cc5b3ec73e3ee734
Stephane Menard <stephane@friendstrip.com>
2019-11-05 15:37:58 -0500

    ActionChargingPointNearMetro

--- actions.py
+++ actions.py
@@ -38,26 +38,23 @@ class ActionChargingPointNearMetro(Action):
         return "action_charging_point_near_metro"
 
     @staticmethod
-    def normalize_metro_name(name):
-        return name.title()
-
-    @staticmethod
-    def count_charging_point_near_metro(graph, city_name):
-        return graph.run("MATCH (City {name:{cityName}})<-[:IN*]-(p:ChargingPoint) RETURN count(p)",
-                         cityName=city_name).evaluate()
+    def count_charging_point_near_metro(graph, metro_name):
+        # Charging parks at less than 500m of Pie-IX metro station
+        return graph.run("MATCH (MetroStation {name:{metroName}})-[n:NEARBY]-(p:ChargingPark) WHERE n.distance_km < 0.5 RETURN count(p)",
+                         metroName=metro_name).evaluate()
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        metro_name = self.normalize_metro_name(tracker.get_slot('metro')) if tracker.get_slot('metro') else None
+        metro_name = tracker.get_slot('metro')
 
         if metro_name:
-            # graph = Graph(password='abcd')
-            # count = self.count_charging_point_near_metro(graph, metro_name)
-            #
-            # dispatcher.utter_message(f"Il y a {count} bornes de recharge près du métro {metro_name}.")
-            dispatcher.utter_message(f"Les bornes pres du metro ({metro_name}) sont:")
+            graph = Graph(password='abcd')
+            count = self.count_charging_point_near_metro(graph, metro_name)
+
+            dispatcher.utter_message(f"Il y a {count} endroits à moins de 500m du métro {metro_name}.")
+            # dispatcher.utter_message(f"Les bornes pres du metro <{metro_name}> sont:")
         else:
             dispatcher.utter_message(f"Pour quel métro voulez-vous connaitre les bornes de recharge?")
 
